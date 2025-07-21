de33273146229d7109765e51a46f7bf6ddc38f93
monjurelahe <monjureelahe@gmail.com>
2025-07-21 15:51:54 +0600

    Monjur E ELahe

--- lib/feature/forecast_screen.dart
+++ lib/feature/forecast_screen.dart
@@ -48,32 +48,34 @@ class ForeCast extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 13),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.arrow_back_ios, color: Colors.white),
-                  _buildContainer(
-                    temperature: '19\u00B0C',
-                    day: 'Mon',
-                    imagePath: Assetpath.sun,
-                  ),
-                  _buildContainer(
-                    temperature: '18\u00B0C',
-                    day: 'Tue',
-                    imagePath: Assetpath.rain,
-                  ),
-                  _buildContainer(
-                    temperature: '18\u00B0C',
-                    day: 'Wed',
-                    imagePath: Assetpath.rain,
-                  ),
-                  _buildContainer(
-                    temperature: '19\u00B0C',
-                    day: 'Thu',
-                    imagePath: Assetpath.sun,
-                  ),
-                  Icon(Icons.arrow_forward_ios, color: Colors.white),
-                ],
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.arrow_back_ios, color: Colors.white),
+                    _buildContainer(
+                      temperature: '19\u00B0C',
+                      day: 'Mon',
+                      imagePath: Assetpath.sun,
+                    ),
+                    _buildContainer(
+                      temperature: '18\u00B0C',
+                      day: 'Tue',
+                      imagePath: Assetpath.rain,
+                    ),
+                    _buildContainer(
+                      temperature: '18\u00B0C',
+                      day: 'Wed',
+                      imagePath: Assetpath.rain,
+                    ),
+                    _buildContainer(
+                      temperature: '19\u00B0C',
+                      day: 'Thu',
+                      imagePath: Assetpath.sun,
+                    ),
+                    Icon(Icons.arrow_forward_ios, color: Colors.white),
+                  ],
+                ),
               ),
               // SizedBox(
               //   height: 200,
@@ -153,6 +155,73 @@ class ForeCast extends StatelessWidget {
                       borderRadius: BorderRadius.circular(19),
                       gradient: LinearGradient(
                         colors: [Color(0xff3E2D8F), Color(0xff9D52AC)],
+                      ),
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.8),
+                        width: 0.5,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image(image: AssetImage(Assetpath.crosshairs)),
+                              SizedBox(width: 55),
+                              Text('SUNRISE', style: AppFontStyle.smallerText),
+                            ],
+                          ),
+                          SizedBox(height: 11),
+                          Text('5:28 AM', style: AppFontStyle.subHeading),
+                          SizedBox(height: 11),
+                          Text(
+                            'Sunset: 7.25PM',
+                            style: AppFontStyle.smallerText,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  //2nd Container
+                  Container(
+                    height: 150,
+                    width: 165,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(19),
+                      gradient: LinearGradient(
+                        colors: [Color(0xff3E2D8F), Color(0xff9D52AC)],
+                      ),
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.8),
+                        width: 0.5,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image(image: AssetImage(Assetpath.crosshairs)),
+                              SizedBox(width: 55),
+                              Text('UV INDEX', style: AppFontStyle.smallerText),
+                            ],
+                          ),
+                          SizedBox(height: 11),
+                          Text('4', style: AppFontStyle.subHeading),
+                          //SizedBox(height: 11),
+                          Text('Moderate', style: AppFontStyle.smallerText),
+                        ],
                       ),
                     ),
                   ),
